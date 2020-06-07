149a44611a6a81e7d2c16835efe0e80fc0595c09
Stanko Bebek <stanko.bebek82@gmail.com>
2020-06-07 22:30:48 +0200

    added footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-      <h1>Footer</h1>
+  <div class="footer">
+      <p>Copyright © 2020 Informatičko-tehnička udruga SWITCH. Sva prava pridržana.</p>
   </div>
 </template>
 
@@ -10,6 +10,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.footer{
+    display: flex;           
+    flex-direction: column;  
+    justify-content: center; 
+    align-items: center;
+    height: 50px;
+    width: 100vw;
+    background: #1f4386;
+}
+
+.footer p{ 
+    color: white;
+}
 
 </style>

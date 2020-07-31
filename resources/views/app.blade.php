05e60b19f7834c102d4fc348e9c5933951e9a188
Stanko Bebek <stanko.bebek82@gmail.com>
2020-07-31 21:50:48 +0200

    fix3

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Informatika</title>
+    <title>informatika</title>
     <link rel="stylesheet" href="dist/app.css" />
 
     <link rel="icon" href="<%= BASE_URL %>favicon.ico">
@@ -17,6 +17,6 @@
 <body>
     <div id="app"></div>
     
-    <script src="dist/app.js?version=6"></script>
+    <script src="dist/app.js?version=7"></script>
 </body>
 </html>
